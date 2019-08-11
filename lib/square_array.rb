05abf48bffa94806acadf90e3b8ab442356c7f06
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 20:19:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+
+  while counter < array.count do
+    p array[counter]**2
+    counter +=1
+  end
 end
+
+square_array([1,2,3])
+square_array([9,10,16,25])
